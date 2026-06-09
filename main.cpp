7c6303ee2ff6f88a83419ab999b2fd9d16b48da5
mirekzn <miroslawzn@yahoo.com>
2026-06-08 22:02:20 -0700

    My changes

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <vector>
+#include "add.h"
+
+using namespace std;
+
+int add(int a, int b);
+
+int main()
+{
+    cout << "ppp" << endl;
+    std::cout << add(2, 3) << '\n';
+}
+
+
+    
+
+
